128e9921f919fe88b27b7ed6015fd6565d517b4b
Your Name <your_email@example.com>
2025-10-01 16:31:08 +0300

    bunner complete

--- netflix-web/src/Components/Banner/Bunner.js
+++ netflix-web/src/Components/Banner/Bunner.js
@@ -1,53 +1,68 @@
-import { useEffect,useState } from "react";
-import "../utils/axios";
-import "../utils/requests";
-import "./bunner.css"
+import { useEffect, useState } from "react";
+
+import axios from "../utils/axios"; 
+import requests from "../utils/requests";
+import "./bunner.css";
 
 
 const Bunner = () => {
-    const [movie,setMoveie]=useState({});
-    useEffect( ()=>{
-        (async ()=> {
-            try{
-            const request=await axios.get(requests.fetchNetflixOriginals)
-            setMoveie(request.data.result[
-                Math.floor(Math.random()* request.data.results.length)
-            ]);
-            } catch(error){
-                console.log("error",error);
+    const [movie, setMovie] = useState({});
+
+    useEffect(() => {
+        const fetchMovie = async () => { 
+            try {
+                const request = await axios.get(requests.fetchNetflixOriginal);
+                console.log(request);
+                
+                
+                setMovie(
+                    request.data.results[
+                        Math.floor(Math.random() * request.data.results.length)
+                        
+                    ]
+                );
+            } catch (error) {
+                console.log("error fetching movie data:", error);
             }
-        })();
+        };
 
-    },[]);
-    return (
-        <div className="banner"
-        style={{
-            backgroundSize:"cover",
-            backgroundImage:`url()`,
-            backgroundPosition:"center",
-            backgroundRepeat:"no-repeat"
-        }}>
-        <div className="banner_contents">
-            <h1 className="banner_title">
-                {movie?.title || movie?.name || movie?.original_name}
-
-            </h1>
-            <div className="banner_button">
-                <button className="banner_button_play">Play</button>
-                <button className="banner_button">My List</button>
-
-
-            </div>
-            {/* <h1 className="banner_description">{truncate(movie?.overview,150)}</h1> */}
-
-        </div>
-        <div className="banner_fadeBottom">
-            
-        </div>
-
-        </div>
-)
+        fetchMovie();
+    }, []); 
+    function truncate(str,n){
+        return str?.length> n? str.substr(0,n-1)+"...." :str;
     }
 
+    const getBannerImageUrl = (path) => {
+        const baseImageUrl = "https://image.tmdb.org/t/p/original/";
+        return path ? `${baseImageUrl}${path}` : ''; 
+    };
+
+    return (
+        <header className="banner" 
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url(${getBannerImageUrl(movie?.backdrop_path)})`,
+                backgroundPosition: "center center",
+                backgroundRepeat: "no-repeat"
+            }}
+        >
+            <div className="banner_contents">
+                <h1 className="banner_title">
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
+                
+                <div className="banner_buttons">
+                    <button className=" play banner_button">Play</button>
+                    <button className="banner_button mylist">My List</button>
+                </div>
+
+                <h1 className="banner_description">{truncate(movie?.overview,150)}
+                </h1>
+
+            </div>
+            <div className="banner_fadeBottom" />
+        </header>
+    );
+}
 
 export default Bunner;
--- netflix-web/src/Components/utils/requests.js
+++ netflix-web/src/Components/utils/requests.js
@@ -1,7 +1,12 @@
-const API_KEY=ProcessingInstruction.env.REACT_APP_KEY;
+const API_KEY = process.env.REACT_APP_KEY
 
 const requests={
 
+    fetchTrending:`/trending/all/week?api_key=${API_KEY} &language=en-US`,
+    fetchNetflixOriginal:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRatedMovies:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`
+    
 }
 
 export default requests;
